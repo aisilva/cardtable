e7912eec8a308754caece41b850c5034dedc54b7
Adrian Silva <aisilva@mit.edu>
2020-07-18 18:43:05 -0400

    Testing messaging

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import os
 
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'beep':
+        await message.channel.send('boop')
+
+    if message.content == 'cards':
+        await message.channel.send('🂳 🂴 🂵 ')
+            
+
+#class CustomClient(discord.Client):
+#    async def on_ready(self):
+#        print(f'{client.user} has connected to Discord!')
+    
+client.run(TOKEN)

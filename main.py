53f73ab6d93b15b0c37614eb3e112f61f412d81f
Adrian Silva <aisilva@mit.edu>
2020-10-14 14:18:55 -0400

    Fixed discard

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ bot = commands.Bot(command_prefix=';')
 players = {}
 
 deck = pydealer.Deck()
-deck.shuffle()
+#deck.shuffle()
 discard_pile = pydealer.Stack()
 
 
@@ -33,7 +33,7 @@ async def table(ctx):
         await ctx.send(str(len(deck)) + " cards in deck, 0 cards in discard" )
 
         
-    await ctx.send('\n'.join( [str(player)+': '+str(players[player][1]) for player in players] ))
+    await ctx.send('\n'.join( [str(player)+': '+str(players[player][1]) + "+ " + str(len(players[player][0])) + ' facedown cards' for player in players] ))
 
 
 @bot.command(name='addme', help='adds player to game')
@@ -54,31 +54,67 @@ async def hand(ctx):
 @bot.command(name='draw', help='draws a card from discard or deck')
 async def draw(ctx, num: int, pile: str):
     if pile == 'discard':
-        temp = discard_pile.deal(num)
+        temp = list(discard_pile.deal(num))
     else: #LAZY
-        temp = deck.deal(num)
+        temp = list(deck.deal(num))
 
     
-    players[str(ctx.author)][0] += [temp[i] for i in range(len(temp))]
+    #players[str(ctx.author)][0] += [temp[i] for i in range(len(temp))]
+    players[str(ctx.author)][0] += temp
     await hand(ctx)
 
 @bot.command(name='discard', help='moves a card to discard pile')
-async def discard(ctx, ind: int):
-    disc = players[str(ctx.author)][0][ind]
-    players[str(ctx.author)][0].remove(disc)
-    discard_pile.add(disc)
+async def discard(ctx, which: str, ind):
+    #which is 'hand' or 'table'
+    if which == 'hand':
+        which = 0
+    elif which == 'table':
+        which = 1
+    else:
+        ctx.send('error')
+        return
+    
+    if ind == 'all':
+        for card in players[str(ctx.author)][which]:
+            discard_pile.add(card)
+        players[str(ctx.author)][which] = []
 
+    else:
+        disc = players[str(ctx.author)][which][int(ind)]
+        players[str(ctx.author)][which].remove(disc)
+        discard_pile.add(disc)
+
+
+    await table(ctx)
+
+    
+
+
+    
 @bot.command(name='play', help='moves a card to table')
 async def play(ctx, ind: int):
     disc = players[str(ctx.author)][0][ind]
     players[str(ctx.author)][0].remove(disc)
     players[str(ctx.author)][1].append(disc)
-
-@bot.command(name='shuffle', help='shuffles discard into deck')
-async def shuffle(ctx):
-    deck.shuffle
+    await table(ctx)
     
+@bot.command(name='shuffle', help='shuffles discard into deck. Add `False` to leave discard pile as-is')
+async def shuffle(ctx, add_discard=True):
+    if add_discard== True:
+        temp = discard_pile.deal(len(discard_pile))
+        deck.add(temp)
+        deck.shuffle()
+    else:
+        deck.shuffle()
     
+@bot.command(name='deal', help='deals X cards to each player in game')
+async def deal(ctx, num: int):
+    for i in range(num):
+        for player in players:
+            temp=list(deck.deal(1))
+            players[player][0] += temp
+    await table(ctx)
+    await hand(ctx)
 
 @bot.event
 async def on_ready():
